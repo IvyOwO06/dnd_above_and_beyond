73931c4c392817abf6123fa331c9c283783cebfc
Aphroditeskiss <94465@roc-teraa.nl>
2025-06-02 11:35:29 +0200

    FIX HOVER

--- blogpost.php
+++ blogpost.php
@@ -2,12 +2,22 @@
 $blogId = $_GET['id'];
 require 'inc/navFunctions.php';
 require 'inc/blogFunctions.php';
+?>
+<head>
+    <!DOCTYPE html>
+    <html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<?php
+displayHeader();
 
 displayBlog($blogId); 
 submitComment($blogId);
-displayHeader();
 ?>
-
 <h2>Submit a Comment</h2>
 <form class="form" method="POST" action="blogpost.php?id=<?php echo $blogId; ?>">
     <label for="author">Your Name:</label>
