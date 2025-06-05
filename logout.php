99095b9c710034a02fa5f01cfd19ce0b8ba9bf21
Sam <samdelhaye06@gmail.com>
2025-06-05 09:20:50 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-require_once 'vendor/autoload.php';
 require_once 'inc/functions.php';
 session_destroy();
 echo '<script>alert("Logged out successfully!"); window.location.href = "index.php";</script>';
